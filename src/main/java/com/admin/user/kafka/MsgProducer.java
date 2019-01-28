f36917c7c73b28fc188b5afbe01456fd50e91d45
chigin <chigin@outlook.com>
2019-01-28 14:35:07 +0800

    提供生产

--- src/main/java/com/admin/user/kafka/MsgProducer.java
+++ src/main/java/com/admin/user/kafka/MsgProducer.java
@@ -1,46 +1,52 @@
 package com.admin.user.kafka;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
-import kafka.javaapi.producer.Producer;
-import kafka.producer.KeyedMessage;
-import kafka.producer.ProducerConfig;
-import kafka.serializer.StringEncoder;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class MsgProducer extends Thread{
-    private String topic;
-    public MsgProducer(String topic){
-        super();
-        this.topic = topic;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.PostConstruct;
+
+public class MsgProducer {
+     @Autowired
+    private KafkaConfig kafkaConfig;
+    private KafkaProducer  msgProducer;
+    private ExecutorService executorService;
+
+    private KafkaProducer createProducer(){
+        Properties properties=new Properties();
+        properties.put("bootStrap.servers",kafkaConfig.getBroker());
+        properties.put("group.id",kafkaConfig.getProducerGroup());
+        properties.put("auto.commit.interval.ms",kafkaConfig.getCommitInterval());
+        properties.put("enable.auto.commit","true");
+        properties.put("key.serializer", StringSerializer.class.getName());
+        properties.put("value.serializer", StringSerializer.class.getName());
+        properties.put("key.desreializer","org.apache.kafka.common.serialization.StringDesreializer");
+        properties.put("value.desreializer","org.apache.kafka.common.serialization.StringDesreializer");
+        properties.put("partition.assigment.strategy","range");
+        return new KafkaProducer<>(properties);
     }
 
-
-    public void run() {
-        Producer<Integer, String> producer = createProducer();
-        int i=0;
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
-        while(true){
-            producer.send(new KeyedMessage<Integer, String>(topic, "时间："+ df.format(new Date())+",message: " + i++));
-            System.out.println("发送时间："+ df.format(new Date())+",message: " + i);
-            try {
-                TimeUnit.SECONDS.sleep(1);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-    private Producer<Integer, String> createProducer() {
-        Properties properties = new Properties();
-        properties.put("zookeeper.connect", "39.106.135.201:2181");//zookeeper安装在机器IP
-        properties.put("serializer.class", StringEncoder.class.getName());
-        properties.put("metadata.broker.list", "39.106.135.201:9092");// kafka安装的机器IP
-        return new Producer<Integer, String>(new ProducerConfig(properties));
+    @PostConstruct
+    public void init(){
+        executorService = Executors.newFixedThreadPool(kafkaConfig.getProducerThreadNum());
+        msgProducer =createProducer();
     }
 
-    public static void main(String[] args) {
-        new MsgProducer("test").start();
+    /**
+     * 发送消息
+     * @param message
+     */
+    public void service(String message){
+        executorService.execute(()->{
+            msgProducer.send(new ProducerRecord<>(kafkaConfig.getTopic(),null,message));
+        });
     }
+
 }
 
 
