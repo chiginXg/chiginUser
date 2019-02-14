0c6bbdb5b4f9df72328183c5cbbd6125a4060fc7
吴刚 <wugang@thunisoft.com>
2019-02-14 15:02:09 +0800

    {*}新增swagger

---
+++ src/main/java/com/admin/user/swagger/Swagger2.java
@@ -0,0 +1,44 @@
+package com.admin.user.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author: wugang
+ * @version: 1.0
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                //为当前包路径
+                .apis(RequestHandlerSelectors.basePackage("com.admin.user.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+   private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                //页面标题
+                .title("用户api接口")
+                //创建人
+                .contact(new Contact("wugang", "http://www.baidu.com", ""))
+                //版本号
+                .version("1.0")
+                //描述
+                .description("API 描述")
+                .build();
+    }
+}
